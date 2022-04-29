9699987a0399850b586929d34041e11b26dc49d2
asop321 <asop321@gmail.com>
2022-04-29 10:59:30 +0900

    첫번째 커밋

---
+++ js/slider.js
@@ -0,0 +1,21 @@
+$( document ).ready( function() {
+    $( '.slider' ).bxSlider({
+      auto: true,
+      pause: 3000,
+    });
+  } );
+
+  $( document ).ready( function() {
+    $( '.main-slider' ).bxSlider( {
+      auto: true,
+      pause: 2000,
+      autoHover: true,
+      minSlides: 5,
+      maxSlides: 5,
+      slideWidth: 800,
+      captions: false,
+      infiniteLoop: true,
+      moveSlides:1
+     
+    } );
+  } );
